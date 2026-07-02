2bd22fd3130671c119a66c8f87f743c94f8521f7
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-07-02 23:43:12 +0100

    style: jsdoc/reject-function-type

--- packages/indiekit/lib/post-template.js
+++ packages/indiekit/lib/post-template.js
@@ -10,9 +10,9 @@ const jsonPostTemplate = (properties) => {
 /**
  * Get post template
  * @param {object} publication - Publication configuration
- * @param {Function} [publication.postTemplate] - Publication post template
+ * @param {function(object): string} [publication.postTemplate] - Publication post template
  * @param {object} [publication.preset] - Publication preset
- * @returns {Function} Post template rendering function
+ * @returns {function(object): string} Post template rendering function
  */
 export const getPostTemplate = ({ postTemplate, preset }) => {
   if (postTemplate) {
